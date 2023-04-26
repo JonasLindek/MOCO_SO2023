30bfbf10a2f6b2452c59c795f9193bdbbb6a25b6
nezbite <nesbite85@gmail.com>
2023-04-26 12:18:26 +0200

    added heart, improved gui look, cleanup code

--- SocialPet/app/src/main/java/com/datojo/socialpet/MainActivity.kt
+++ SocialPet/app/src/main/java/com/datojo/socialpet/MainActivity.kt
@@ -3,27 +3,31 @@ package com.datojo.socialpet
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import com.datojo.socialpet.ui.theme.SocialPetTheme
-import kotlin.math.round
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -63,10 +67,17 @@ fun MenuOverlay() {
     ) {
         // TODO: Healthbars
         StatusBar(Color.Red, 0.5f)
-        StatusBar(Color.Yellow, 0.75f)
-        StatusBar(Color.Green, 1f)
+        StatusBar(Color.Green, 0.75f)
+        StatusBar(Color(.2f,.4f,1f), 1f)
         Row() {
-            FloatingActionButton(onClick = { /*TODO*/ }) {
+            FloatingActionButton(
+                onClick = { /*TODO*/ },
+                shape = CircleShape,
+                containerColor = Color.White,
+                modifier = Modifier
+                    .width(75.dp)
+                    .height(75.dp)
+            ) {
 
             }
         }
@@ -75,8 +86,27 @@ fun MenuOverlay() {
 
 @Composable
 fun StatusBar(color: Color, state: Float) {
-    LinearProgressIndicator(
-        progress = state,
-        color = color,
-        modifier = Modifier.width(54.dp).padding(0.dp, 15.dp))
+    Row(
+        modifier = Modifier.padding(0.dp, 4.dp)
+    ) {
+        Image(
+            painter= painterResource(id = R.drawable.heart),
+            contentDescription = "Heart",
+            modifier = Modifier
+                .width(28.dp)
+                .height(28.dp)
+                .zIndex(1f)
+        )
+        LinearProgressIndicator(
+            progress = state,
+            color = color,
+            modifier = Modifier
+                .width(54.dp)
+                .padding(0.dp, 12.dp)
+                .height(12.dp)
+                .absoluteOffset((-10).dp, (-4).dp)
+                .border(2.dp, Color.Black, MaterialTheme.shapes.small)
+                .clip(MaterialTheme.shapes.small)
+        )
+    }
 }
