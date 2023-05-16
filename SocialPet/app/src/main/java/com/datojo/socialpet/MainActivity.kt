52f8eff4a4db12a6cdd0c38a1f5f4646c38f0fad
JonasLindek <jonas@fam-lindek.de>
2023-05-16 22:06:17 +0200

    Added Popup Menu, and Screens for Arcade and Mall

--- SocialPet/app/src/main/java/com/datojo/socialpet/MainActivity.kt
+++ SocialPet/app/src/main/java/com/datojo/socialpet/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -21,6 +22,9 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
@@ -35,8 +39,9 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
 import androidx.compose.ui.zIndex
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
@@ -54,10 +59,11 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+//Class to simplify Navigation by storing the route names and adding a feature to add arguments
 sealed class Screen(val route: String) {
     object HomeScreen : Screen("home_screen")
-    object FriendScreen : Screen("friend_screen")
-    object SettingsScreen : Screen("settings_screen")
+    object FriendListScreen : Screen("friend_screen")
     object ArcadeScreen : Screen("arcade_screen")
     object MallScreen : Screen("mall_screen")
 
@@ -69,6 +75,8 @@ sealed class Screen(val route: String) {
     }
 }
 
+
+//Navigation by Navcontroller
 @Composable
 fun Navigation() {
     val navController = rememberNavController()
@@ -76,42 +84,116 @@ fun Navigation() {
         composable(route = Screen.HomeScreen.route) {
             HomeScreen(navController = navController)
         }
-        composable(route = Screen.FriendScreen.route) {
+        composable(route = Screen.FriendListScreen.route) {
             FriendListScreen(navController = navController)
+        }
+        composable(route = Screen.ArcadeScreen.route) {
+            ArcadeScreen(navController = navController)
+        }
+        composable(route = Screen.MallScreen.route) {
+            MallScreen(navController = navController)
         }
     }
 }
 
+
+//Composables listing all needed parts for the different Screens
 @Composable
 fun HomeScreen(navController : NavController) {
     SocialPetTheme {
-        Background()
+        Background(R.drawable.bedroom, "Bedroom",
+            Modifier
+                .scale(3.8f)
+                .padding(0.dp, 275.dp)
+        )
         CatAnimation()
-        MenuOverlay { navController.navigate(Screen.FriendScreen.route) }
-    }
-}
+        MenuOverlay({ navController.navigate(Screen.FriendListScreen.route) },
+            listOf(
 
-@Composable
-fun Background() {
-    Image(
-        painter = painterResource(id = R.drawable.bedroom),
-        contentDescription = "Bedroom",
-        modifier = Modifier
-            .scale(3.8f)
-            .padding(0.dp, 275.dp)
-    )
+                { navController.navigate(Screen.ArcadeScreen.route) },
+                { navController.navigate(Screen.MallScreen.route) }
+            ),
+            listOf(
+                "Arcade",
+                "Mall"
+            )
+        )
+    }
 }
 
 @Composable
 fun FriendListScreen(navController: NavController) {
     val contacts = listOf("Daniel Sonnenberg", "Tom Küper", "Jonas Lindek")
     SocialPetTheme {
-        ListOverlay(contacts, { navController.navigate(Screen.HomeScreen.route) }, { 1 + 1})
+        ListOverlay(contacts, { navController.popBackStack() }, { 1 + 1})
     }
 }
 
 @Composable
-fun MenuOverlay(onClick: () -> Unit) {
+fun ArcadeScreen(navController: NavController) {
+    SocialPetTheme {
+        Background(R.drawable.arcade, "Arcade",
+            Modifier
+                .scale(3.8f)
+                .fillMaxSize()
+                .padding(0.dp, 275.dp)
+        )
+        MenuOverlay({ navController.navigate(Screen.FriendListScreen.route) },
+            listOf(
+                { navController.navigate(Screen.HomeScreen.route) },
+                { navController.navigate(Screen.MallScreen.route) }
+            ),
+            listOf(
+                "Home",
+                "Mall"
+            )
+        )
+    }
+}
+
+@Composable
+fun MallScreen(navController: NavController) {
+    SocialPetTheme {
+        Background(R.drawable.mall, "Mall",
+            Modifier
+                .scale(3.8f)
+                .fillMaxSize()
+                .padding(0.dp, 275.dp)
+        )
+        MenuOverlay({ navController.navigate(Screen.FriendListScreen.route) },
+            listOf(
+                { navController.navigate(Screen.HomeScreen.route) },
+                { navController.navigate(Screen.ArcadeScreen.route) }
+            ),
+            listOf(
+                "Home",
+                "Arcade"
+            )
+        )
+    }
+}
+
+@Composable
+fun Background(id: Int, description: String, modifier: Modifier) {
+    Image(
+        painter = painterResource(id = id),
+        contentDescription = description,
+        modifier = modifier
+    )
+}
+
+
+//Overlays to add to the Screens
+@Composable
+fun MenuOverlay(friendRoute: () -> Unit, screenRoutes: List<() -> Unit>, screenNames: List<String>) {
+    var menuPopUpControl by remember { mutableStateOf(false) }
+    var notificationPopUpControl by remember { mutableStateOf(false) }
+    var audioPopUpControl by remember { mutableStateOf(false) }
+    var aboutPopUpControl by remember { mutableStateOf(false) }
+
+    if(menuPopUpControl)
+        MenuPopUp({ change -> menuPopUpControl = change }, friendRoute, screenRoutes, screenNames)
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -124,7 +206,7 @@ fun MenuOverlay(onClick: () -> Unit) {
         StatusBar(Color.Green, 0.75f)
         StatusBar(Color(.2f,.4f,1f), 1f)
         Row {
-            MenuButton(onClick)
+            MenuButton { change -> menuPopUpControl = change }
         }
     }
 }
@@ -153,7 +235,7 @@ fun ListOverlay(contacts: List<String>, onClick: () -> Unit, addContact: () -> U
 }
 
 
-
+//Composable Parts to add to the overlays
 @Composable
 fun StatusBar(color: Color, state: Float) {
     Row(
@@ -182,19 +264,61 @@ fun StatusBar(color: Color, state: Float) {
 }
 
 @Composable
-fun MenuButton(onClick: () -> Unit) {
-    Row (){
-        FloatingActionButton(
-            onClick = onClick ,
-            shape = CircleShape,
-            containerColor = Color.White,
-            modifier = Modifier
-                .width(75.dp)
-                .height(75.dp)
-        ) {
+fun MenuPopUp(onDismiss: (Boolean) -> Unit, friendRoute: () -> Unit, screenRoutes: List<() -> Unit>, screenNames: List<String>){
+    Column(
+        modifier = Modifier
+            .background(Color.LightGray.copy(alpha = .5f))
+    ) {
+        Popup(
+            onDismissRequest = { onDismiss(false) },
+            properties = PopupProperties(
+                focusable = true,
+                dismissOnClickOutside = true,
 
+            )
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(100.dp, 170.dp)
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(Color.LightGray.copy(alpha = .5f)),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Top
+
+            ) {
+                Spacer(Modifier.padding(6.dp))
+                Text(
+                    text = "Go Somewhere",
+                    color = Color.White,
+                    textAlign = TextAlign.Center
+                )
+
+                PopUpButton(onClick = friendRoute, name = "Friends")
+
+                PopUpButton(onClick = screenRoutes[0], name = screenNames[0])
+
+                PopUpButton(onClick = screenRoutes[1], name = screenNames[1])
+
+                PopUpButton(onClick = { /*TODO*/ }, name = "Settings")
+            }
         }
     }
+}
+
+@Composable
+fun SettingsPopUp() {
+
+}
+
+@Composable
+fun NotificationPopUp() {
+
+}
+
+@Composable
+fun AboutPopUp() {
+
 }
 
 @Composable
@@ -258,20 +382,37 @@ fun ContactBar(onClick: () -> Unit) {
                 .fillMaxWidth()
                 .padding(horizontal = 8.dp),
             horizontalArrangement = Arrangement.Start
-        ) {
+        ){
             BackButton(onClick)
         }
+
         Row(
             modifier = Modifier
                 .fillMaxHeight(),
             verticalAlignment = Alignment.CenterVertically
         ){
-
-
             Text(
                 text = "Contacts",
                 textAlign = TextAlign.Center,
             )
+        }
+    }
+}
+
+
+//Button Composables
+@Composable
+fun MenuButton(onClick: (Boolean) -> Unit) {
+    Row(){
+        FloatingActionButton(
+            onClick = { onClick(true) } ,
+            shape = CircleShape,
+            containerColor = Color.White,
+            modifier = Modifier
+                .width(75.dp)
+                .height(75.dp)
+        ) {
+
         }
     }
 }
@@ -304,6 +445,26 @@ fun AddButton(onClick: () -> Unit) {
                 .height(50.dp)
         ) {
 
+        }
+    }
+}
+
+@Composable
+fun PopUpButton(onClick: () -> Unit, name: String) {
+    Row() {
+        Button(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(70.dp)
+                .padding(12.dp, 6.dp),
+            onClick = onClick,
+            colors = ButtonDefaults.buttonColors(containerColor = Color.LightGray.copy(alpha = .7f)),
+            shape = RoundedCornerShape(5.dp)
+        ) {
+            Text(
+                text = name,
+                color = Color.White
+            )
         }
     }
 }
