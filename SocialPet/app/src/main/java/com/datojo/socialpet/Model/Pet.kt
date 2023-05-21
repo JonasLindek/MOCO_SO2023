9bbfa2a50debe18fe17844435453d3a57a9dc2ce
nezbite <nesbite85@gmail.com>
2023-05-21 13:43:31 +0200

    added Pet model class

---
+++ SocialPet/app/src/main/java/com/datojo/socialpet/Model/Pet.kt
@@ -0,0 +1,10 @@
+package com.datojo.socialpet.Model
+
+class Pet(
+    val name: String,
+    val breed: String,
+    val age: Int,
+    var health: Float,
+    var hunger: Float,
+    var social: Float
+)
